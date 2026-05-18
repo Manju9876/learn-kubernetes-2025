a85648ae0deee40e1defac693226cd91689ddeb5
Manju9876 <manjunathb960650@gmail.com>
2026-05-18 15:34:23 +0530

    github actions practice

--- wmp-v1/main.tf
+++ wmp-v1/main.tf
@@ -83,7 +83,7 @@ resource "aws_eks_node_group" "main" {
   node_group_name = "example"
   node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = ["subnet-005ab3b734b47f3f7", "subnet-068ce337c8cfe6696"]
-  instance_types = ["t3.micro"]
+  instance_types = ["t3.small"]
 
   scaling_config {
     desired_size = 1
